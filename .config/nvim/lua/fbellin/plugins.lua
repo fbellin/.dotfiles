6024295942feec0eb30a67c0ac13aeed8bd02304
François Bellin <francois@bellin.me>
2022-02-25 14:36:01 +0100

    Adds comment plugin

--- .config/nvim/lua/fbellin/plugins.lua
+++ .config/nvim/lua/fbellin/plugins.lua
@@ -81,6 +81,9 @@ return packer.startup(function(use)
 	-- Git
 	use "lewis6991/gitsigns.nvim"
 
+	-- Comments
+	use "numToStr/Comment.nvim"
+
 	-- Automatically set up your configuration after cloning packer.nvim
 	-- Keep this at the end afeter all plugins
 	if PACKER_BOOTSTRAP then
