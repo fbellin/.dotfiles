734531d76231c4b213eb5d53e429c65fb1c872f2
François Bellin <francois@bellin.me>
2022-03-08 13:43:45 +0100

    Adds Rust plugins for nvim

--- .config/nvim/lua/fbellin/lsp/lsp-installer.lua
+++ .config/nvim/lua/fbellin/lsp/lsp-installer.lua
@@ -28,6 +28,11 @@ lsp_installer.on_server_ready(function(server)
 		opts = vim.tbl_deep_extend("force", pyright_opts, opts)
 	end
 
+	if server.name == "rust_analyzer" then
+		local rust_opts = require("fbellin.lsp.settings.rust_analyzer")
+		opts = vim.tbl_deep_extend("force", rust_opts, opts)
+	end
+
 	-- This setup() function is exactly the same as lspconfig's setup function.
 	-- Refer to https://github.com/neovim/nvim-lspconfig/blob/master/doc/server_configuration.md
 	server:setup(opts)
