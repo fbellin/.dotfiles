182f102a28bc501d970a1b81086b92f96af9e706
François Bellin <francois@bellin.me>
2025-02-08 15:36:30 +0100

    Dashboard configuration ok

--- nvim/.config/nvim/lua/fbellin/dashboard.lua
+++ nvim/.config/nvim/lua/fbellin/dashboard.lua
@@ -3,29 +3,64 @@ if not status_ok then
 	vim.notify("Error while loading dashboard configuration")
 end
 
+vim.api.nvim_set_hl(0, "DashboardHeader", { fg = "#99a4bc", bold = true })
+vim.api.nvim_set_hl(0, "DashboardCenterIcon", { fg = "#ff761a", bold = true })
+vim.api.nvim_set_hl(0, "DashboardCenterText", { fg = "#ffc552", bold = true })
+
+local menu = {
+    { "", "Update Packages", "u", ":PackerUpdate" },
+    { "", "Find files", "f", ":Telescope find_files"},
+    { "", "Live Grep", "g", ":Telescope live_grep" },
+		{ "", "Git log", "s", ":Telescope git_status" },
+    { "", "Recently used files", "r", ":Telescope oldfiles" },
+    { "", "Quit Neovim", "q", ":qa" }
+}
+
+local function transform(entries)
+	local transformed = {}
+	for _, entry in ipairs(entries) do
+		table.insert(transformed, {
+			icon = entry[1] .. "  ",
+			icon_hl = "DashboardCenterText",
+			desc = entry[2],
+			desc_hl = "DashboardHeader",
+			key = entry[3],
+			key_hl = "DashboardCenterIcon",
+			action = entry[4]
+		})
+	end
+  return transformed
+end
+
 dashboard.setup({
 	theme = "doom",
 	config = {
 		header = {
-'                                       ',
-'              ██ █████ ██              ',
-'              ██ █████ ██              ',
-'             ▐██ █████ ██▌             ',
-'             ▐██ █████ ██▌             ',
-'             ███ █████ ███             ',
-'             ███ █████ ███             ',
-'            ▐███ █████ ███▌            ',
-'           ▐████ █████ ████▌           ',
-'          ▐████▌ █████ ▐████▌          ',
-'         ▐█████  █████  █████▌         ',
-'        ██████▌  █████  ▐██████        ',
-'      ███████▌   █████   ▐███████      ',
-'   ████████▌     █████     ▐████████   ',
-'█████████▌       █████       ▐█████████',
-'██████▌          █████          ▐██████',
-'                                       ',
-'                                       ',
+	[[                                                                       ]],
+	[[                                                                       ]],
+	[[                                                                       ]],
+	[[                                                                       ]],
+	[[                                                                       ]],
+	[[                                                                     ]],
+	[[       ████ ██████           █████      ██                     ]],
+	[[      ███████████             █████                             ]],
+	[[      █████████ ███████████████████ ███   ███████████   ]],
+	[[     █████████  ███    █████████████ █████ ██████████████   ]],
+	[[    █████████ ██████████ █████████ █████ █████ ████ █████   ]],
+	[[  ███████████ ███    ███ █████████ █████ █████ ████ █████  ]],
+	[[ ██████  █████████████████████ ████ █████ █████ ████ ██████ ]],
+	[[                                                                       ]],
+	[[                                                                       ]],
+	[[                                                                       ]],
+	[[                                                                       ]],
+	[[                                                                       ]],
+	[[                                                                       ]],
+	[[                                                                       ]],
+	[[                                                                       ]],
 		},
+		center = transform(menu)
 	}
 })
+
+
 
