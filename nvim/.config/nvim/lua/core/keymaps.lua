c4bcfa6438b837dcedd0aa1e2536263e65e91e5d
Francois Bellin <francois@bellin.me>
2025-12-31 11:52:04 +0100

    Switch to Omarchy

--- nvim/.config/nvim/lua/core/keymaps.lua
+++
@@ -1,6 +0,0 @@
-vim.g.mapleader = " "
-
-local keymap = vim.keymap.set
-
--- Opens Lazy
-keymap("n", "<Leader>l", ":Lazy<CR>")
--- nvim/.config/nvim/lua/plugins/plenary.lua
+++
@@ -1,3 +0,0 @@
-return {
-  "nvim-lua/plenary.nvim"	-- useful functions used by a lot of plugins
-}
